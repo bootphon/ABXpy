f5fc8138b0448b552f5dc0d49e584f2fd3055b24
Thomas Schatz <thomas.schatz@laposte.net>
2014-03-25 14:55:22 +0100

    suppressing old directory for DTW install

--- ABXpy/distances/distances/src/compile_dtw.sh
+++
@@ -1,8 +0,0 @@
-# find script directory
-folder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# create .c and .so
-python compile_dtw.py build_ext --build-lib $folder/ --build-temp $folder/tmp/
-
-# clean-up temp
-rm -Rf $folder/tmp/
