fab62d130d85f3c08712b46b4f8f6732c7c0b3d4
Thomas Schatz <thomas.schatz@laposte.net>
2014-03-25 17:59:25 +0100

    again trying to make dtw installation callable from another directory

--- ABXpy/distances/metrics/install/compile_dtw.py
+++ ABXpy/distances/metrics/install/compile_dtw.py
@@ -8,8 +8,9 @@ Created on Mon Oct 28 11:30:48 2013
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
-import numpy
+import numpy, os
+path = os.path.dirname(os.path.realpath(__file__))
 
-extension = Extension("dtw", ["dtw.pyx"], extra_compile_args=["-O3"], include_dirs=[numpy.get_include()])
+extension = Extension("dtw", [os.path.join(path, "dtw.pyx")], extra_compile_args=["-O3"], include_dirs=[numpy.get_include()])
 
 setup(name = "DTW implementation in cython", ext_modules = cythonize(extension))
