7e68bbecc48d22ed46bf0ccaa3a01ed3560fbf0d
syhw <gabriel.synnaeve@gmail.com>
2014-07-21 14:01:36 +0200

    added float32 support for cosine distances

--- ABXpy/distances/metrics/cosine.py
+++ ABXpy/distances/metrics/cosine.py
@@ -12,7 +12,7 @@ import scipy
 # x and y should be 2D numpy arrays with "features" on the lines and "times" on the columns
 # x, y must be float arrays    
 def cosine_distance(x,y):
-    assert x.dtype == np.float64 and y.dtype == np.float64
+    assert (x.dtype == np.float64 and y.dtype == np.float64) or (x.dtype == np.float32 and y.dtype == np.float32)
     x2 = np.sqrt(np.sum(x**2, axis=1))
     y2 = np.sqrt(np.sum(y**2, axis=1))
     ix = x2 == 0.
@@ -27,4 +27,11 @@ def cosine_distance(x,y):
     d[:, iy] = 1.
     d[ix, iy] = 0.
     assert np.all(d>=0)
-    return d  
+    return d
+
+
+def normalize_cosine_distance(x, y):
+    x /= x.sum(1).reshape(x.shape[0], 1)
+    y /= y.sum(1).reshape(y.shape[0], 1)
+    return cosine_distance(x, y)
+
