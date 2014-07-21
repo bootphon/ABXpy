corrects a bug in the KL non null sum assert

ABXpy/distances/metrics/kullback_leibler.py
@@ -28,7 +28,7 @@ def kl_divergence(x, y, thresholded=True, symmetrized=True, normalize=True):
      - normalize=True => normalize the inputs so that lines sum to one.
     """
     assert (x.dtype == np.float64 and y.dtype == np.float64) or (x.dtype == np.float32 and y.dtype == np.float32)
-    assert (x.sum(1) != 0. and y.sum(1) != 0.)
+    assert (np.all(x.sum(1) != 0.) and np.all(y.sum(1) != 0.))
     if thresholded:
         normalize = True
     if normalize:
@@ -52,7 +52,7 @@ def js_divergence(x, y, normalize=True):
      - normalize=True => normalize the inputs so that lines sum to one.
     """
     assert (x.dtype == np.float64 and y.dtype == np.float64) or (x.dtype == np.float32 and y.dtype == np.float32)
-    assert (x.sum(1) != 0. and y.sum(1) != 0.)
+    assert (np.all(x.sum(1) != 0.) and np.all(y.sum(1) != 0.))
     if normalize:
         x /= x.sum(1).reshape(x.shape[0], 1)
         y /= y.sum(1).reshape(y.shape[0], 1)