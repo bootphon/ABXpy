567bc3ba54de4606770b336a3c54c350f1bbacf5
Roland Thiolliere <rolthiolliere@gmail.com>
2014-08-19 19:40:21 +0200

    Revert "removed pep8 warning"
    This reverts commit 07ba28516f6ef556aa2a0534e9dc7cf3ab822297.

--- ABXpy/sampling/__init__.py
+++ ABXpy/sampling/__init__.py
@@ -9,15 +9,3 @@ Created on Sun Aug 18 22:31:59 2013
 
 This file only serves to signal that the content of the folder is a Python package.
 """
-"""This module implement an incremental sampler used to approximate the task
-and randomly select a portion of the triplets.
-"""
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 18 22:31:59 2013
-
-@author: thomas
-
-This file only serves to signal that the content of the folder is a Python
-package.
-"""
