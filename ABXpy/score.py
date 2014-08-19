003409416066efcfd8a6bf05a63d25113fe0768a
Roland Thiolliere <rolthiolliere@gmail.com>
2014-08-19 18:14:52 +0200

    added pep8 formating and file checking for score

--- ABXpy/score.py
+++ ABXpy/score.py
@@ -19,7 +19,8 @@ In python:
     import ABXpy.task
     import ABXpy.score
     # create a new task:
-    myTask = ABXpy.task.Task('data.item', 'on_feature', 'across_feature', 'by_feature', filters=my_filters, regressors=my_regressors)
+    myTask = ABXpy.task.Task('data.item', 'on_feature', 'across_feature', \
+'by_feature', filters=my_filters, regressors=my_regressors)
     myTask.generate_triplets()
     #initialise distance
     #TODO shouldn't this be available from score
@@ -65,32 +66,53 @@ def score(task_file, distance_file, score_file=None, score_group='scores'):
         (basename_task, _) = os.path.splitext(task_file)
         (basename_dist, _) = os.path.splitext(distance_file)
         score_file = basename_task + '_' + basename_dist + '.score'
+    # file verification:
+    assert os.path.exists(task_file), 'Cannot find task file ' + task_file
+    assert os.path.exists(distance_file), ('Cannot find distance file ' +
+                                           distance_file)
+    assert not os.path.exists(score_file), ('score file already exist ' +
+                                            score_file)
     #with h5py.File(task_file) as t:
         #bys = [by for by in t['triplets']]
-    #FIXME skip empty by datasets, this should not be necessary anymore when empty datasets are filtered at the task file generation level
+    #FIXME skip empty by datasets, this should not be necessary anymore when
+    # empty datasets are filtered at the task file generation level
     with h5py.File(distance_file) as d:
         bys = [by for by in d['distances']]
     for by in bys:
         with h5py.File(task_file) as t, h5py.File(distance_file) as d:
             n = t['triplets'][by].shape[0]
-            dis = d['distances'][by][...]  #FIXME here we make the assumption that this fits into memory ...
+            #FIXME here we make the assumption
+            # that this fits into memory ...
+            dis = d['distances'][by][...]
             dis = np.reshape(dis, dis.shape[0])
-            pairs = t['unique_pairs'][by][...] #FIXME idem + only unique_pairs used ?
+            #FIXME idem + only unique_pairs used ?
+            pairs = t['unique_pairs'][by][...]
             pairs = np.reshape(pairs, pairs.shape[0])
             base = t['unique_pairs'].attrs[by]
-            pair_key_type = type_fitting.fit_integer_type((base)**2-1, is_signed=False)
+            pair_key_type = type_fitting.fit_integer_type((base)**2-1,
+                                                          is_signed=False)
         with h52np.H52NP(task_file) as t:
             with np2h5.NP2H5(score_file) as s:
                 inp = t.add_dataset('triplets', by)
-                out = s.add_dataset('scores', by, n_rows=n, n_columns=1, item_type=np.int8)
-                try: # FIXME replace this by a for loop by making h52np implement the iterable pattern with next() outputing inp.read()          
-                    while True: #FIXME keep the pairs in the file ?                 
+                out = s.add_dataset('scores', by, n_rows=n, n_columns=1,
+                                    item_type=np.int8)
+                try:  # FIXME replace this by a for loop by making h52np
+                # implement the iterable pattern with next() outputing
+                # inp.read()
+                    while True:  # FIXME keep the pairs in the file ?
                         triplets = pair_key_type(inp.read())
-                        pairs_AX = triplets[:,0]+base*triplets[:,2]
-                        pairs_BX = triplets[:,1]+base*triplets[:,2] #FIXME change the encoding (and type_fitting) so that A,B and B,A have the same code ... (take a=min(a,b), b=max(a,b))
+                        pairs_AX = triplets[:, 0]+base*triplets[:, 2]
+                        #FIXME change the encoding (and type_fitting) so that
+                        # A,B and B,A have the same code ... (take a=min(a,b),
+                        # b=max(a,b))
+                        pairs_BX = triplets[:, 1]+base*triplets[:, 2]
                         dis_AX = dis[np.searchsorted(pairs, pairs_AX)]
                         dis_BX = dis[np.searchsorted(pairs, pairs_BX)]
-                        scores = np.int8(dis_AX < dis_BX) - np.int8(dis_AX > dis_BX) # 1 if X closer to A, -1 if X closer to B, 0 if equal distance (this doesn't use 0, 1/2, 1 to use the compact np.int8 data format)
+                        scores = (np.int8(dis_AX < dis_BX) -
+                                  np.int8(dis_AX > dis_BX))
+                        # 1 if X closer to A, -1 if X closer to B, 0 if equal
+                        # distance (this doesn't use 0, 1/2, 1 to use the
+                        # compact np.int8 data format)
                         out.write(np.reshape(scores, (scores.shape[0], 1)))
                 except StopIteration:
                     pass
@@ -101,7 +123,10 @@ if __name__ == '__main__':
 
     import argparse
 
-    # parser (the usage string is specified explicitly because the default does not show that the mandatory arguments must come before the mandatory ones; otherwise parsing is not possible beacause optional arguments can have various numbers of inputs)
+    # parser (the usage string is specified explicitly because the default
+    # does not show that the mandatory arguments must come before the mandatory
+    # ones; otherwise parsing is not possible beacause optional arguments can
+    # have various numbers of inputs)
     parser = argparse.ArgumentParser(usage="%(prog)s task distance [score]",
                                      description='ABX score computation')
     # I/O files
