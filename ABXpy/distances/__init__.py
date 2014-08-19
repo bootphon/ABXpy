4b1fdd7653ec5ce56b61dedde294c1352b86ebe8
Roland Thiolliere <rolthiolliere@gmail.com>
2014-08-19 19:40:29 +0200

    Revert "removed pep8 warning"
    This reverts commit 679cfdd43f48bfc8186d00ec601764d50ec8c3ee.

--- ABXpy/distances/__init__.py
+++ ABXpy/distances/__init__.py
@@ -7,12 +7,3 @@ Created on Sun Aug 18 22:31:59 2013
 This file only serves to signal that the content of the folder is a Python package.
 """
 
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 18 22:31:59 2013
-
-@author: thomas
-
-This file only serves to signal that the content of the folder is a Python package.
-"""
-
