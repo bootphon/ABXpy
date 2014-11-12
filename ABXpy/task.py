356e0c0048c4a52ea89dbc275acdab5134897cdf
THIOLLIERE <thiolliere@thiolliere-Aspire-7740.(none)>
2014-11-12 20:40:21 +0100

    corrected features file related warning in task

--- ABXpy/task.py
+++ ABXpy/task.py
@@ -994,9 +994,11 @@ or phonemes, if your database contains columns defining these attributes)"""
                     help='optional: feature file, verify the consistency '
                          'of the feature file with the item file')
     args = parser.parse_args()
+    if args.stats_only:
+        assert args.output, "The output file was not provided"
     if not args.stats_only and os.path.exists(args.output):
         print("WARNING: Overwriting task file " + args.output)
-    if not args.no_verif and args.features:
+    if not args.no_verif and (not args.features or not os.path.exists(args.features)):
         print("WARNING: Cannot verify the consistency of the item file {0} "
               "with the features file because the features file was not "
               "provided")
