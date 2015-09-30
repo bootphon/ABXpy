7d2a7febfcb6240b76d703dec1ec1423438fee59
Mathieu Bernard <mathieu_bernard@laposte.net>
2015-09-30 17:56:31 +0200

    Refactoring and testing of database.py.
    The _load_aux_databases have still to be improved and tested.

--- ABXpy/database/database.py
+++ ABXpy/database/database.py
@@ -1,97 +1,229 @@
-"""Load a database from disk."""
-import os
-import sys
-import pandas
+"""Provides the load() function for loading an ABX database from disk."""
+
+import logging
 import numpy
+import os.path
+import pandas
+import tinytree
+#import ABXpy.misc.tinytree as tinytree
 
-import ABXpy.misc.tinytree as tinytree
+
+def load(filename, features_info=False):
+    """Load an ABX database from its filename.
+
+    An ABX database indexes the database on which the ABX task is
+    executed.  See doc/FileFormat.rst for more details.
+
+    Parameters:
+
+    filename -- str. The file name of the ABX database to load.
+    Following ABX conventions the extension of that file should be
+    .item but this is not requested by the function.
+
+    features_info -- bool. If True return features related columns
+    from the database. False by default.
+
+    Return:
+
+    TODO:
+
+    Raise:
+
+    IOError if the file cannot be loaded or is badly formatted.
+
+    """
+    # check the requested file exists
+    if not os.path.isfile(filename):
+        raise IOError('The file {} cannot be loaded'.format(filename))
+
+    # load the database from pandas (can raise)
+    db = _load_database(filename)
+
+    # split database into features and attribute columns (can raise)
+    if features_info:
+        attribute_db, feature_db = _split_database(db, filename, True)
+    else:
+        attribute_db = _split_database(db, filename, False)
+
+    logging.info('ABX database loaded from {}.'.format(filename))
+
+    # merge existing auxiliary files with the main database. Create a
+    # hierarchy at the same time (useful for optimizing regressors and
+    # filters in ABX.task)
+    attribute_db, db_hierarchy = _load_aux_databases(attribute_db, filename)
+
+    # detecting missing items
+    nan_rows = numpy.any(pandas.isnull(attribute_db), 1)
+    is_nan = any(nan_rows)
+
+    # TODO: feature_db is not dropped. Should it be done?
+    # TODO: hierarchy is not updated, is it a bug?
+    if is_nan:
+        # suppress missing items in attributes
+        attribute_db = attribute_db[~ nan_rows]
+
+        # create a file for dropping
+        (base, ext) = os.path.splitext(filename)
+        drop_file = base + '-removed' + ext
+
+        # drop them to a separate file.
+        dropped = attribute_db[nan_rows]
+        dropped.to_csv(drop_file)
+
+        logging.warning('{} items were removed '.format(len(nanrows)) +
+                        'because of missing information. The ' +
+                        'removed items are listed in {}.'.format(drop_file))
+
+    # removing missing items from feature_db if needed
+    if features_info and is_nan:
+        feature_db = feature_db[~ nan_rows]
+
+    if features_info:
+        return attribute_db, db_hierarchy, feature_db
+    else:
+        return attribute_db, db_hierarchy
 
 
-# FIXME: use just one isolated | as a separator instead of two #
+def _load_database(filename):
+    """Load the raw database with pandas.
 
-# custom read_table that ignore empty entries at the end of a file (they
-# can result from trailing white spaces at the end for example)
-def read_table(filename):
+    This function delegate loading to pandas.read_table(). Empty
+    entries at the end of the file are ignored.
+
+    Parameters:
+
+    filename : str -- The file to load the database from.
+
+    Return:
+
+    The loaded database as a panda DataFrame.
+
+    """
     db = pandas.read_table(filename, sep='[ \t]+')
-    # removes row with all null values (None or NaN...)
+
+    # removes all null values (None or NaN...)
     db = db.dropna(how='all')
+
     return db
 
-# function that loads a database
 
-# TODO: properly deals with errors on loading file
-def load(filename, features_info=False):
-    # reading the main database using pandas (it is now a DataFrame)
-    ext = '.item'
-    if not(filename[len(filename) - len(ext):] == ext):
-        filename = filename + ext
-    db = read_table(filename)
+def _split_database(database, filename, features_info):
+    """Split features columns from attribute columns in a database.
 
-    # finding '#' (to separate location info from attribute info) and fixing
-    # names of columns
-    columns = db.columns.tolist()
-    l = []
-    for i, c in enumerate(columns):
-        if c[0] == "#":
-            l.append(i)
-            columns[i] = c[1:]
-    db.columns = pandas.Index(columns)
-    assert len(l) > 0 and l[
-        0] == 0, ('The first column name in the database main file must be '
-                  'prefixed with # (sharp)')
-    assert len(
-        l) == 2, ('Exactly two column names in the database main file must be'
-                  ' prefixed with a # (sharp)')
-    feat_db = db[db.columns[:l[1]]]
-    db = db[db.columns[l[1]:]]
-    # verbose print("  Read input File '"+filename+"'. Defined conditions:
-    # "+str(newcolumns[attrI:len(columns)]))
+    This function parse the 1st line of the database and split it
+    according to the position of the second '#' found in column
+    names. Before that separator are features, after are attributes.
 
-    # opening up existing auxiliary files, and merging with the main database
-    # and creating a forest describing the hierarchy at the same time (useful
-    # for optimizing regressor generation and filtering)
+    Parameters:
 
-    (basename, _) = os.path.splitext(filename)
-    db, db_hierarchy = load_aux_dbs(basename, db, db.columns, filename)
+    database : pandas.DataFrame -- The database to split.
 
-    # dealing with missing items: for now rows with missing items are dropped
-    nanrows = numpy.any(pandas.isnull(db), 1)
-    if any(nanrows):
-        dropped = db[nanrows]
-        dropped.to_csv(basename + '-removed' + ext)
-        db = db[~ nanrows]
-    feat_db = feat_db[~ nanrows]
-    # not so verbose print('** Warning ** ' + len(nanrows) + ' items were
-    # removed because of missing information. The removed items are listed in'
-    # + basename + '-removed.item'
+    filename : str -- The file where the database was loaded
+    from. This is usefull only for raising exceptions.
+
+    features_info -- bool. If True return features related columns
+    from the database.
+
+    Return:
+
+    The attribute columns of the database as a panda DataFrame. If
+    features_info is True, also return the features columns, also as a
+    DataFrame. Names of the columns are cleaned (with the '#'
+    removed).
+
+    Raise:
+
+    IOError if the header line is badly formatted (if 1st column is
+    not prefixed with '#' and if there is not exactly 2 columns
+    begining with '#'.
+
+    """
+    # column titles as a str list
+    columns = database.columns.tolist()
+
+    # check for badly formatted titles
+    if not columns[0][0] == '#':
+        raise IOError('The first column in {}'.format(filename) +
+                      ' must be prefixed with #')
+
+    if not ''.join(columns).count('#') == 2:
+        raise IOError('Exactly two columns in {}'.format(filename) +
+                      ' must be prefixed with #')
+
+    # find column indexes begining with '#'
+    sharp_list = []
+    for index, name in enumerate(columns):
+        if name[0] == "#":
+            sharp_list.append(index)
+            columns[index] = name[1:]
+
+    # rename columns with removed '#'
+    database.columns = pandas.Index(columns)
+
+    # split the database
+    attribute_db = database[database.columns[sharp_list[1]:]]
+
     if features_info:
-        return db, db_hierarchy, feat_db
+        feature_db = database[database.columns[:sharp_list[1]]]
+        return attribute_db, feature_db
     else:
-        return db, db_hierarchy
+        return attribute_db
 
 
-# recursive auxiliary function for loading the auxiliary databases
-def load_aux_dbs(basename, db, cols, mainfile):
-    forest = [tinytree.Tree() for col in cols]
-    for i, col in enumerate(cols):
+def _load_aux_databases(database, filename):
+    """Load auxiliary databases.
+
+    This function is a wrapper to _load_aux_databases_rec().
+
+    """
+    # filename with extension removed
+    basename = os.path.splitext(filename)[0]
+
+    # start the recursion
+    database, forest = _load_aux_databases_rec(database, database.columns,
+                                               filename, basename)
+
+    return database, forest
+
+
+# TODO: document and test this function!
+def _load_aux_databases_rec(database, columns, filename, basename):
+    """Recursive function for loading auxiliary databases.
+
+    """
+    # Init an empty tree per column
+    forest = [tinytree.Tree() for col in columns]
+
+    for i, col in enumerate(columns):
         forest[i].name = col
+        aux_file = basename + '.' + col
+
+        # TODO: replace try/except by os.path.isfile when tested
         try:
-            auxfile = basename + '.' + col
-            if not(auxfile == mainfile):
-                auxdb = read_table(auxfile)
-                assert col == auxdb.columns[0], (
-                    'First column name in file %s'
-                    ' is %s. It should be %s instead.' % (
-                        auxfile, auxdb.columns[0], col))
-                # call get_aux_dbs on child columns
-                auxdb, auxforest = load_aux_dbs(
-                    basename, auxdb, auxdb.columns[1:])
+            if not aux_file == filename:
+                aux_db = _load_database(aux_file)
+
+                # TODO: replace assert by raise
+                assert col == aux_db.columns[0], (
+                    'First column name in file {}'
+                    ' is {}. It should be {} instead.'.format(
+                        aux_file, aux_db.columns[0], col))
+
+                # recursive call on child columns
+                aux_db, aux_forest = _load_aux_databases_rec(aux_db,
+                                                             aux_db.columns[1:],
+                                                             filename,
+                                                             basename)
                 # add to forest
-                forest[i].addChildrenFromList(auxforest)
+                forest[i].addChildrenFromList(aux_forest)
+
                 # merging the databases
-                db = pandas.merge(db, auxdb, on=col, how='left')
-        # verbose print("  Read auxiliary File '"+auxfile+"'. Defined
-        # conditions: "+str(newcol[1:len(newcol)])+" on key '"+newcol[0]+"'")
+                database = pandas.merge(database, aux_db, on=col, how='left')
+
+                logging.info('Read auxiliary file {}. '.format(aux_file) +
+                             'Defined conditions {} on key {}.'.format(
+                                 str(newcol[1:len(newcol)]), newcol[0]))
         except IOError:
             pass
-    return db, forest
+
+    return database, forest
---
+++ ABXpy/test/test_database.py
@@ -0,0 +1,77 @@
+"""This script tests the database.py file"""
+
+import os
+import pandas.core.frame
+import pytest
+
+import ABXpy.misc.items as items
+import ABXpy.database.database as database
+
+class TestDatabase:
+    """Test of the database ABX module"""
+
+    def setup(self):
+        self.features_info = True
+        self.filename = '/tmp/data.item'
+        items.generate_named_testitems(2, 3, name=self.filename)
+
+        self.db = database._load_database(self.filename)
+
+    def teardown(self):
+        try:
+            os.remove(self.filename)
+        except(OSError):
+            pass
+
+    def test_load_database(self):
+        assert type(self.db) == pandas.core.frame.DataFrame
+
+        assert ''.join(self.db.columns.tolist()).count('#') == 2
+        assert self.db.columns[0][0] == '#'
+
+        with pytest.raises(IOError):
+            database._load_database('/path/to/no/where')
+
+    def test_split_database_good(self):
+        dba, dbf = database._split_database(self.db,self.filename,
+                                            self.features_info)
+
+        assert dba.columns.tolist() == ['item', 'c0', 'c1', 'c2']
+        assert dbf.columns.tolist() == ['file', 'onset', 'offset']
+        assert len(dba.index) == len(dbf.index) == 8
+
+    def test_split_database_nofeatures1(self):
+        with pytest.raises(ValueError):
+            dba, dbf = database._split_database(self.db,self.filename, False)
+
+    def test_split_database_nofeatures2(self):
+        dba = database._split_database(self.db,self.filename, False)
+        assert dba.columns.tolist() == ['item', 'c0', 'c1', 'c2']
+        assert len(dba.index) == 8
+
+    def test_split_database_badsharp1(self):
+        db_bad = self.db.rename(columns={'#file':'truc'})
+        assert not db_bad.columns[0][0] == '#'
+
+        with pytest.raises(IOError) as err:
+            dba, dbf = database._split_database(db_bad,self.filename,
+                                                self.features_info)
+        assert 'The first column in' in str(err.value)
+
+    def test_split_database_badsharp2(self):
+        db_bad = self.db.rename(columns={'#item':'truc'})
+        assert ''.join(db_bad.columns.tolist()).count('#') == 1
+
+        with pytest.raises(IOError) as err:
+            dba, dbf = database._split_database(db_bad,self.filename,
+                                                self.features_info)
+        assert 'Exactly two columns in' in str(err.value)
+
+    def test_split_database_badsharp3(self):
+        db_bad = self.db.rename(columns={'c0':'#c0'})
+        assert ''.join(db_bad.columns.tolist()).count('#') == 3
+
+        with pytest.raises(IOError) as err:
+            dba, dbf = database._split_database(db_bad,self.filename,
+                                                self.features_info)
+        assert 'Exactly two columns in' in str(err.value)
